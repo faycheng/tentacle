5f7be42fdd72285e16135cd0392af4b2f8e4f5bc
faycheng <fei.cheng@daocloud.io>
2017-06-09 22:28:52 +0800

    add search command

--- tentacle.py
+++ tentacle.py
@@ -3,6 +3,7 @@
 import os
 import json
 import click
+from terminaltables import GithubFlavoredMarkdownTable as Table
 from hub.hub import Hub
 
 CONFIG_PATH = os.path.expanduser('~') + '/'
@@ -11,8 +12,31 @@ CONFIG = CONFIG_PATH + CONFIG_NAME
 CTX = {}
 
 
+def pretty_table(data, filter=None):
+    headers = []
+    for item in data:
+        keys = list(item.keys())
+        headers = list(set(headers + keys))
+    if filter is not None:
+        assert isinstance(filter, list)
+        headers = [header for header in headers if header not in filter]
+    rows = []
+    for item in data:
+        assert isinstance(item, dict)
+        row = []
+        for header in headers:
+            row.append(item.get(header, ''))
+        rows.append(row)
+    table_data = list()
+    table_data.append(headers)
+    table_data.extend(rows)
+    table = Table(table_data)
+    return table.table
+
+
 @click.group(chain=True)
 def cli():
+    global CTX
     if not os.path.exists(CONFIG):
         with open(CONFIG, 'w') as fd:
             json.dump({
@@ -45,6 +69,14 @@ def login():
         fd.seek(0)
         json.dump(config, fd, indent=4)
     click.echo('Login Success')
+
+
+@cli.command()
+@click.argument('query', type=str)
+def search(query):
+    res = Hub().search(CTX['token'], query)
+    click.echo('Count:{}'.format(res['count']))
+    click.echo(pretty_table(res['results'], filter=['repo_owner', 'short_description']))
 
 
 if __name__ == '__main__':
